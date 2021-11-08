4da5b9933d3ae4d4f48813e48a1ff67061a22707
Gabrielle Pangilinan <92441760+gabrielle0@users.noreply.github.com>
2021-11-08 12:27:10 +0800

    2nc commit

--- Program 3.py
+++ Program 3.py
@@ -3,11 +3,19 @@ def enter_Money_Apple ():
     apple_Price = int (input("How much is an apple? "))
     return amount_Money, apple_Price
 
+def max_Apples (money, apples):
+    max_Apples_ = money // apples
+    return max_Apples_
 
+def remaining_Money (money, apples):
+    remaining_Money_ = money % apples
+    return remaining_Money_
 
 
 #Steps:
 #1.) Enter amount of money and price of an apple. Save to variables. 
 money, apples  = enter_Money_Apple ()
 #2.) Calculate maximum number of apples and remaining money. 
+maximum_Apples = max_Apples (money, apples)
+change = remaining_Money (money, apples)
 #3.) Display the maximum number of apples that you can buy and the remaining money that you will have.
