69d7fcc75a9ce166a976e448042b0c0fdfbc9fdf
Gabrielle Pangilinan <92441760+gabrielle0@users.noreply.github.com>
2021-11-08 12:23:55 +0800

    2nd commit

--- Program 2.py
+++ Program 2.py
@@ -5,7 +5,9 @@ def get_Amount_Apples_Oranges():
     oranges = oranges_Func*25
     return apples, oranges 
 
-
+def get_Total (apples, oranges):
+    total = apples + oranges
+    return total
 
 
 
@@ -14,4 +16,5 @@ def get_Amount_Apples_Oranges():
 #1.) Ask for amount of apples and oranges you want to buy. Save to variables. Compute respective cost.
 apples, oranges = get_Amount_Apples_Oranges ()
 #2.) Add total apples and oranges. Save total to variable.
+total = get_Total (apples, oranges)
 #3.) Display the output in the following format: The total amount is_ 
